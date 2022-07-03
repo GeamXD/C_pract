c999aef82def707425c8011acf8dc0ce4e8ce080
Musa Godwin <musa.godwin8112>
2022-07-03 07:02:46 +0100

    details

---
+++ basic_declar/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - Prints out user details
+  *
+  *Return: returns 0 if successsful
+  */
+
+int main(void)
+{
+	printf("Name   : Musa Godwin\n");
+	printf("DOB    : OCT 1, 2000\n");
+	printf("Mobile   : 08137590665\n");
+
+	return (0);
+}
