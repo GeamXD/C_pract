3846d4a7efe32c5f995af81fa64a8576efae61ae
Musa Godwin <musa.godwin8112>
2022-07-03 07:32:08 +0100

    prints hash and in reverse

---
+++ basic_declar/2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  *main - prints hash
+  *
+  *Return: 0 if successful
+  */
+
+int main(void)
+{
+	printf("######");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+
+
+	printf("\n");
+
+	printf(" ######\n");
+	printf("##     ##\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("##     ##\n");
+	printf(" ######\n");
+
+	return (0);
+}
---
+++ basic_declar/3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - prints in reverse
+  *
+  *Return: 0 if successful
+  */
+
+int main(void)
+{
+	char x = 'X';
+	char m = 'M';
+	char l = 'L';
+
+	printf("The reverse of XML is: %c%c%c\n", l, m, x);
+	return (0);
+}
